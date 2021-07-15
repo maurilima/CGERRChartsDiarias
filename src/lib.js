1e52d9c0f0de7118773287d64fc1df66fa9ba81d
maurilima <mauri.lima.roraima@gmail.com>
2021-07-15 12:59:02 -0400

    Testando leitura key

--- src/lib.js
+++ src/lib.js
@@ -2,31 +2,22 @@ let h = new Headers();
 // myHeaders.append("Content-Type", "application/json");
 
 const BASE_URL = 'http://aplicacoes.segad.rr.gov.br:8080/transparencia/oauth/token'
-let userPw = 'ewfWEF55465#@#';
+let userPw = 'ewfWEF55465#@#',
 var Usename = 'portal_transparencia'
 let encoded = btoa(userPw+':'+Usename);
 let auth = 'Basic ' + encoded;
-let formdata = new FormData();
-
-
-
-formdata.append('grant_type','client_credentials');
-// formdata.append('username','testname');
 
 
 h.append('Accept', 'application/json');
 h.append('Authorization', auth );
-h.append( 'Content-Type','application/x-www-form-urlencoded')
 console.log(auth);
 
 let req = new Request(BASE_URL, {
-    method: 'POST',
+    method: 'GET',
     headers:h,
-    body : formdata
- 
+    credentials : 'same-origin'
 
 })
-
 
 
 
@@ -38,3 +29,20 @@ fetch(req)
 .catch(err => {
   console.error(err);
 });
+
+
+export async function getDataApi(data){
+    let raw = JSON.stringify(data)
+    var requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow'
+    }; 
+    try {
+        let res = await fetch(BASE_URL, requestOptions);
+        return await res.json();
+    } catch (error) {
+        console.log(error)
+    }
+}
