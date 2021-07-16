8fcc94c082114b8036e640eda8fa314c3f4be756
Mauri Lima <mauri.lima.roraima@gmail.com>
2021-07-16 11:29:26 -0400

    Tentando de novo

--- src/config/Config.js
+++
@@ -1,6 +0,0 @@
-export const BASE_URL = 'http://aplicacoes.segad.rr.gov.br:8080/transparencia/oauth/token';
-let userPw = 'ewfWEF55465#@#';
-var userName = 'portal_transparencia';
-export let encoded = btoa(userName+':'+userPw);
-export let AUTH = 'Basic ' + encoded;
-
