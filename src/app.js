5d77a675a66f065a7ed52d856e73f519647bc174
Mauri Lima <mauri.lima.roraima@gmail.com>
2021-07-17 01:25:29 -0400

    Primeiro Grafico barras Diarias por UO

--- src/app.js
+++ src/app.js
@@ -1,55 +1,72 @@
 import { URL_TDSE } from "./config/Config.js";
 import { getApisEndPoint } from "./libs/getApiEndPoint.js";
 // import { getAuthKey } from "./libs/getApiKey.js";
+let yearToday = new Date().getFullYear();
 
+let divCharts = document.getElementById('charts')
+// console.log(yearToday, divCharts);
+
+
+
+//  document.addEventListener('DOMContentLoaded',function(){
 let raw = {
-     execicio : '2018',
-     limiteDeRegistros : 15
+     execicio: yearToday,
+     limiteDeRegistros: 15
 }
 
-let result = await getApisEndPoint(raw, URL_TDSE);
+// console.log('entra', raw)
+// let retorno = await getApisEndPoint(raw, URL_TDSE);
 
-console.log(result)
+document.addEventListener('DOMContentLoaded', (event) => {
+
+     // console.log('Carregour')
+     // renderChartsBarOrgao()
+     //  console.log(raw)
+
+})
+
+// renderChartsBarOrgao()
+
+//      async function renderChartsBarOrgao(){
+
+let retorno = await getApisEndPoint(raw, URL_TDSE);
+let lSerie = retorno.map(item => item.total)
+let lLabels =retorno.map(item => item.codigoUnidadeorcamentaria) 
+
+let options = {
+     chart: {
+          type: 'bar',
+          width : '90%',
+          height: 600
+     },
+     series: [
+          {
+               name: 'Total diarias',
+
+               data: lSerie
+          }],
+     xaxis: {
+          categories: lLabels
+
+     },
+     grid: {
+          show: true,
+          xaxis:{
+               lines:{
+                    show:true
+               }
+          }
+     },
+     title: {
+          text: 'Top 15 em Diárias',
+          align: 'left'
+
+     }
+
+}
+let chart = new ApexCharts(charts, options)
+chart.render();
 
 
-// let h = new Headers();
-// // myHeaders.append("Content-Type", "application/json");
-
-// const BASE_URL = 'http://aplicacoes.segad.rr.gov.br:8080/transparencia/oauth/token'
-// let userPw = 'ewfWEF55465#@#';
-// var Usename = 'portal_transparencia'
-// let encoded = btoa(Usename+':'+userPw);
-// let auth = 'Basic ' + encoded;
-
-
-
-
-// let raw  =  ['grant_type=client_credentials']
-// h.append('Accept', 'application/json');
-// h.append('Authorization', auth );
-// h.append( 'Content-Type','application/x-www-form-urlencoded')
-// console.log(auth);
-
-// let req = new Request(BASE_URL, {
-//     method: 'POST',
-//     headers:h,
-//     body : raw 
-
-// })
-
-
-
-
-
-// fetch(req)
-// .then(response => {
-//   response.json().then(
-//       json => {
-//           console.log(json);
-      
-//       }
-//   )
-//     })
-// .catch(err => {
-//   console.error(err);
-// });
+          // console.log(retorno);
+     // }      
