e95c2311ae119d7e1e35e7883e45b9f45cbdfc87
Mauri Lima <mauri.lima.roraima@gmail.com>
2021-07-19 10:20:47 -0400

    Gerando Primeiro Charts

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import { URL_TDSE } from "./config/Config.js";
 import { getApisEndPoint } from "./libs/getApiEndPoint.js";
+import { parseFloat2Decimals } from './libs/lib.js'
 // import { getAuthKey } from "./libs/getApiKey.js";
 let yearToday = new Date().getFullYear();
 
@@ -11,62 +12,116 @@ let divCharts = document.getElementById('charts')
 //  document.addEventListener('DOMContentLoaded',function(){
 let raw = {
      execicio: yearToday,
-     limiteDeRegistros: 15
+     limiteDeRegistros: 12
 }
 
 // console.log('entra', raw)
 // let retorno = await getApisEndPoint(raw, URL_TDSE);
 
-document.addEventListener('DOMContentLoaded', (event) => {
 
-     // console.log('Carregour')
+// renderChartsBarOrgao()
+
+//      async function renderChartsBarOrgao(){
+// let lSerie = [];
+// let lLabels = [];
+
+document.addEventListener('DOMContentLoaded', (event) => {
+     console.log(event)
+
+     console.log('Carregour')
      // renderChartsBarOrgao()
      //  console.log(raw)
 
 })
 
-// renderChartsBarOrgao()
-
-//      async function renderChartsBarOrgao(){
 
 let retorno = await getApisEndPoint(raw, URL_TDSE);
-let lSerie = retorno.map(item => item.total)
-let lLabels =retorno.map(item => item.codigoUnidadeorcamentaria) 
+let lSerie = retorno.map(item => parseFloat2Decimals(item.total))
+let lLabels = retorno.map(item => item.codigoUnidadeorcamentaria)
+let legenda = retorno.map(item => {
+     return { codigo: item.codigoUnidadeorcamentaria,
+              descricao: item.descricaoUnidadeOrcamentaria,
+     }}
+);
+// console.log(legenda)
+
+
+
 
 let options = {
      chart: {
           type: 'bar',
-          width : '90%',
-          height: 600
+          width: '90%',
+          height: 400
+     },
+     plotOptions: {
+          bar: {
+               borderRadius: 4,
+               horizontal: false,
+               dataLabels: {
+                    position: 'top', // top, center, bottom
+               },
+          }
      },
      series: [
           {
                name: 'Total diarias',
-
                data: lSerie
           }],
+     dataLabels: {
+          enabled: true,
+          position: 'top',
+          style: {
+               colors: ['#fff'],
+          },
+
+          formatter: function (val, opts) {
+               return val.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })
+
+          },
+          offsetY: -15,
+          style: {
+               fontSize: '11px',
+               colors: ["#304758"]
+          }
+
+
+     },
+
      xaxis: {
-          categories: lLabels
+          categories: lLabels,
 
      },
      grid: {
           show: true,
-          xaxis:{
-               lines:{
-                    show:true
+          xaxis: {
+               lines: {
+                    show: true
                }
           }
      },
      title: {
-          text: 'Top 15 em Diárias',
+          text: 'Top 10 de Gasto com Diárias',
           align: 'left'
 
-     }
+     },
 
 }
+
 let chart = new ApexCharts(charts, options)
 chart.render();
 
 
-          // console.log(retorno);
-     // }      
+// chart.updateSeries(
+//      [
+//           {
+//                data: lSerie
+//           }
+
+//      ])
+
+
+
+
+console.log('Fim');
+     // }
--- src/libs/lib.js
+++ src/libs/lib.js
@@ -1,49 +1,60 @@
-
-// myHeaders.append("Content-Type", "application/json");
-
-// const BASE_URL = 'http://aplicacoes.segad.rr.gov.br:8080/';
-// const END_AUTH = 'transparencia/oauth/token'
-// let userPw = 'ewfWEF55465#@#',
-// var Usename = 'portal_transparencia'
-// let encoded = btoa(userPw+':'+Usename);
-// let auth = 'Basic ' + encoded;
-let h = new Headers();
-
-h.append('Accept', 'application/json');
-h.append('Authorization', auth );
-console.log(auth);
-
-let req = new Request(BASE_URL, {
-    method: 'GET',
-    headers:h,
-    credentials : 'same-origin'
-
-})
-
-
-
-
-fetch(req)
-.then(response => {
-  console.log(response);
-})
-.catch(err => {
-  console.error(err);
-});
-
-
-export async function getDataApi(data){
-    let raw = JSON.stringify(data)
-    var requestOptions = {
-      method: 'POST',
-      headers: myHeaders,
-      body: raw,
-      redirect: 'follow'
-    }; 
-    try {
-        let res = await fetch(BASE_URL, requestOptions);
-        return await res.json();
-    } catch (error) {
-        console.log(error)
-    }
+export function parseFloat2Decimals(value) {
+  if (value != null) {
+      return parseFloat(parseFloat(value).toFixed(2));
+  }
+  else { return 0; }
 }
+
+
+
+//  totalUO.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' }))
+
+
+// // myHeaders.append("Content-Type", "application/json");
+
+// // const BASE_URL = 'http://aplicacoes.segad.rr.gov.br:8080/';
+// // const END_AUTH = 'transparencia/oauth/token'
+// // let userPw = 'ewfWEF55465#@#',
+// // var Usename = 'portal_transparencia'
+// // let encoded = btoa(userPw+':'+Usename);
+// // let auth = 'Basic ' + encoded;
+// let h = new Headers();
+
+// h.append('Accept', 'application/json');
+// h.append('Authorization', auth );
+// console.log(auth);
+
+// let req = new Request(BASE_URL, {
+//     method: 'GET',
+//     headers:h,
+//     credentials : 'same-origin'
+
+// })
+
+
+
+
+// fetch(req)
+// .then(response => {
+//   console.log(response);
+// })
+// .catch(err => {
+//   console.error(err);
+// });
+
+
+// export async function getDataApi(data){
+//     let raw = JSON.stringify(data)
+//     var requestOptions = {
+//       method: 'POST',
+//       headers: myHeaders,
+//       body: raw,
+//       redirect: 'follow'
+//     }; 
+//     try {
+//         let res = await fetch(BASE_URL, requestOptions);
+//         return await res.json();
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
