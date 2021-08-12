1e7c3a73d3c6a928841d72a4c398ca8dcc088745
Mauri Lima <mauri.lima.roraima@gmail.com>
2021-08-11 22:03:42 -0400

    Corrigdio Css em Testes

---
+++ src/testar/UO.js
@@ -0,0 +1,372 @@
+
+
+
+export const listUO =  [
+    {
+        "codigo": "11101",
+        "descricao": "Tribunal de Contas do Estado de Roraima"
+    },
+    {
+        "codigo": "13101",
+        "descricao": "Casa Civil"
+    },
+    {
+        "codigo": "13103",
+        "descricao": "Casa Militar"
+    },
+    {
+        "codigo": "13104",
+        "descricao": "Secretaria de Estado da Comunicação Social"
+    },
+    {
+        "codigo": "15301",
+        "descricao": "Instituto de Previdência do Estado de Roraima"
+    },
+    {
+        "codigo": "16101",
+        "descricao": "Secretaria de Estado de Planejamento e Desenvolvimento"
+    },
+    {
+        "codigo": "16201",
+        "descricao": "Fundação Estadual do Meio Ambiente e Recursos Hídricos de Roraima"
+    },
+    {
+        "codigo": "16401",
+        "descricao": "Agência de Fomento do Estado de Roraima"
+    },
+    {
+        "codigo": "16604",
+        "descricao": "Fundo Estadual do Meio Ambiente"
+    },
+    {
+        "codigo": "17101",
+        "descricao": "Secretaria de Estado da Educação e Desporto"
+    },
+    {
+        "codigo": "18101",
+        "descricao": "Secretaria de Estado da Agricultura, Pecuária e Abastecimento"
+    },
+    {
+        "codigo": "18301",
+        "descricao": "Instituto de Terras e Colonização do Estado de Roraima"
+    },
+    {
+        "codigo": "18302",
+        "descricao": "Agência de Defesa Agropecuária do Estado de Roraima"
+    },
+    {
+        "codigo": "18501",
+        "descricao": "Companhia de Desenvolvimento de Roraima"
+    },
+    {
+        "codigo": "19103",
+        "descricao": "Polícia Militar do Estado de Roraima"
+    },
+    {
+        "codigo": "19105",
+        "descricao": "Polícia Civil do Estado de Roraima"
+    },
+    {
+        "codigo": "19301",
+        "descricao": "Departamento Estadual de Trânsito de Roraima"
+    },
+    {
+        "codigo": "20601",
+        "descricao": "Fundo Estadual de Saúde"
+    },
+    {
+        "codigo": "21101",
+        "descricao": "Secretaria de Estado da Infra-Estrutura"
+    },
+    {
+        "codigo": "22101",
+        "descricao": "Secretaria de Estado da Fazenda"
+    },
+    {
+        "codigo": "22302",
+        "descricao": "Instituto de Pesos e Medidas do Estado de Roraima"
+    },
+    {
+        "codigo": "23101",
+        "descricao": "Secretaria de Estado do Trabalho e Bem-Estar Social"
+    },
+    {
+        "codigo": "23601",
+        "descricao": "Fundo Estadual de Assistência Social"
+    },
+    {
+        "codigo": "25101",
+        "descricao": "Ministério Público do Estado de Roraima "
+    },
+    {
+        "codigo": "26101",
+        "descricao": "Secretaria de Estado da Justiça e Cidadania"
+    },
+    {
+        "codigo": "27101",
+        "descricao": "Secretaria de Estado do Índio"
+    },
+    {
+        "codigo": "30101",
+        "descricao": "Secretaria de Estado de Articulação Municipal e Política Urbana"
+    },
+    {
+        "codigo": "32101",
+        "descricao": "Defensoria Pública do Estado de Roraima"
+    },
+    {
+        "codigo": "33101",
+        "descricao": "Ministério Público de Contas"
+    },
+    {
+        "codigo": "34101",
+        "descricao": "Secretaria de Estado da Cultura"
+    }
+]
+
+
+ 
+
+
+export const UO =  [
+    {
+      "exercicio": 2021,
+      "codigoUnidadeorcamentaria": "11101",
+      "descricaoUnidadeOrcamentaria": "Tribunal de Contas do Estado de Roraima",
+      "nomeCredor": "Manoel Dantas Dias",
+      "cpf": "3118770287",
+      "total": 5901.39
+    },
+    {
+      "exercicio": 2021,
+      "codigoUnidadeorcamentaria": "13101",
+      "descricaoUnidadeOrcamentaria": "Casa Civil",
+      "nomeCredor": "Roberto Barros de Lima",
+      "cpf": "68453590249",
+      "total": 960
+    },
+    {
+      "exercicio": 2021,
+      "codigoUnidadeorcamentaria": "13103",
+      "descricaoUnidadeOrcamentaria": "Casa Militar",
+      "nomeCredor": "Rosivaldo da Silva Pinheiro",
+      "cpf": "32053045272",
+      "total": 240
+    },
+    {
+      "exercicio": 2021,
+      "codigoUnidadeorcamentaria": "13104",
+      "descricaoUnidadeOrcamentaria": "Secretaria de Estado da Comunicação Social",
+      "nomeCredor": "Isaque Lima Santiago",
+      "cpf": "99163381249",
+      "total": 1740
+    },
+    {
+      "exercicio": 2021,
+      "codigoUnidadeorcamentaria": "15301",
+      "descricaoUnidadeOrcamentaria": "Instituto de Previdência do Estado de Roraima",
+      "nomeCredor": "Rafael David Aires Alencar",
+      "cpf": "51299712215",
+      "total": 2850.75
+    },
+    {
+      "exercicio": 2021,
+      "codigoUnidadeorcamentaria": "16101",
+      "descricaoUnidadeOrcamentaria": "Secretaria de Estado de Planejamento e Desenvolvimento",
+      "nomeCredor": "Ronald Brasil Pinheiro",
+      "cpf": "68455321253",
+      "total": 2640
+    },
+    {
+      "exercicio": 2021,
+      "codigoUnidadeorcamentaria": "16201",
+      "descricaoUnidadeOrcamentaria": "Fundação Estadual do Meio Ambiente e Recursos Hídricos de Roraima",
+      "nomeCredor": "Icaron Diego Correa da Rocha",
+      "cpf": "85983187287",
+      "total": 5265
+    },
+    {
+      "exercicio": 2021,
+      "codigoUnidadeorcamentaria": "16401",
+      "descricaoUnidadeOrcamentaria": "Agência de Fomento do Estado de Roraima",
+      "nomeCredor": "Antonio Matos Silva",
+      "cpf": "34621687387",
+      "total": 450
+    },
+    {
+      "exercicio": 2021,
+      "codigoUnidadeorcamentaria": "16604",
+      "descricaoUnidadeOrcamentaria": "Fundo Estadual do Meio Ambiente",
+      "nomeCredor": "Luiz Fernando Melgarejo Avero",
+      "cpf": "38248107272",
+      "total": 8523
+    },
+    {
+      "exercicio": 2021,
+      "codigoUnidadeorcamentaria": "17101",
+      "descricaoUnidadeOrcamentaria": "Secretaria de Estado da Educação e Desporto",
+      "nomeCredor": "Roberto James Nascimento Carvalho",
+      "cpf": "38276070272",
+      "total": 5100
+    },
+    {
+      "exercicio": 2021,
+      "codigoUnidadeorcamentaria": "18101",
+      "descricaoUnidadeOrcamentaria": "Secretaria de Estado da Agricultura, Pecuária e Abastecimento",
+      "nomeCredor": "Samuel Carlos de Santana",
+      "cpf": "43008259715",
+      "total": 1740
+    },
+    {
+      "exercicio": 2021,
+      "codigoUnidadeorcamentaria": "18301",
+      "descricaoUnidadeOrcamentaria": "Instituto de Terras e Colonização do Estado de Roraima",
+      "nomeCredor": "Ronderson Reis Dos Santos",
+      "cpf": "388407239",
+      "total": 4680
+    },
+    {
+      "exercicio": 2021,
+      "codigoUnidadeorcamentaria": "18302",
+      "descricaoUnidadeOrcamentaria": "Agência de Defesa Agropecuária do Estado de Roraima",
+      "nomeCredor": "Jorge Brito Nascimento",
+      "cpf": "89884574200",
+      "total": 6420
+    },
+    {
+      "exercicio": 2021,
+      "codigoUnidadeorcamentaria": "18501",
+      "descricaoUnidadeOrcamentaria": "Companhia de Desenvolvimento de Roraima",
+      "nomeCredor": "Odinei Araujo Pereira",
+      "cpf": "73319678353",
+      "total": 415.91
+    },
+    {
+      "exercicio": 2021,
+      "codigoUnidadeorcamentaria": "19103",
+      "descricaoUnidadeOrcamentaria": "Polícia Militar do Estado de Roraima",
+      "nomeCredor": "Joao da Silva Rocha",
+      "cpf": "85775177215",
+      "total": 2273.48
+    },
+    {
+      "exercicio": 2021,
+      "codigoUnidadeorcamentaria": "19105",
+      "descricaoUnidadeOrcamentaria": "Polícia Civil do Estado de Roraima",
+      "nomeCredor": "Jeaneson de Souza Ferreira",
+      "cpf": "44689934215",
+      "total": 3151.87
+    },
+    {
+      "exercicio": 2021,
+      "codigoUnidadeorcamentaria": "19301",
+      "descricaoUnidadeOrcamentaria": "Departamento Estadual de Trânsito de Roraima",
+      "nomeCredor": "Rodrigo Gomes Carvalho",
+      "cpf": "81561040215",
+      "total": 4140
+    },
+    {
+      "exercicio": 2021,
+      "codigoUnidadeorcamentaria": "20601",
+      "descricaoUnidadeOrcamentaria": "Fundo Estadual de Saúde",
+      "nomeCredor": "Valdiney da Silva Alexandre",
+      "cpf": "44645708287",
+      "total": 7380
+    },
+    {
+      "exercicio": 2021,
+      "codigoUnidadeorcamentaria": "21101",
+      "descricaoUnidadeOrcamentaria": "Secretaria de Estado da Infra-Estrutura",
+      "nomeCredor": "Wendell Gomes Calisto da Costa",
+      "cpf": "74595210",
+      "total": 3480
+    },
+    {
+      "exercicio": 2021,
+      "codigoUnidadeorcamentaria": "22101",
+      "descricaoUnidadeOrcamentaria": "Secretaria de Estado da Fazenda",
+      "nomeCredor": "Antonio da Silva Gomes",
+      "cpf": "70862737249",
+      "total": 9900
+    },
+    {
+      "exercicio": 2021,
+      "codigoUnidadeorcamentaria": "22302",
+      "descricaoUnidadeOrcamentaria": "Instituto de Pesos e Medidas do Estado de Roraima",
+      "nomeCredor": "Ronnie Pereira Lima",
+      "cpf": "38235439204",
+      "total": 4071
+    },
+    {
+      "exercicio": 2021,
+      "codigoUnidadeorcamentaria": "23101",
+      "descricaoUnidadeOrcamentaria": "Secretaria de Estado do Trabalho e Bem-Estar Social",
+      "nomeCredor": "Edna Cristina Gomes Rocha",
+      "cpf": "61333778287",
+      "total": 2696
+    },
+    {
+      "exercicio": 2021,
+      "codigoUnidadeorcamentaria": "23601",
+      "descricaoUnidadeOrcamentaria": "Fundo Estadual de Assistência Social",
+      "nomeCredor": "Gabriella Matias da Silva",
+      "cpf": "86705237287",
+      "total": 120
+    },
+    {
+      "exercicio": 2021,
+      "codigoUnidadeorcamentaria": "25101",
+      "descricaoUnidadeOrcamentaria": "Ministério Público do Estado de Roraima ",
+      "nomeCredor": "MINISTERIO PUBLICO ESTADUAL",
+      "cpf": " ",
+      "total": 1190090
+    },
+    {
+      "exercicio": 2021,
+      "codigoUnidadeorcamentaria": "26101",
+      "descricaoUnidadeOrcamentaria": "Secretaria de Estado da Justiça e Cidadania",
+      "nomeCredor": "Roney Saldanha de Souza Cruz",
+      "cpf": "64541142287",
+      "total": 980
+    },
+    {
+      "exercicio": 2021,
+      "codigoUnidadeorcamentaria": "27101",
+      "descricaoUnidadeOrcamentaria": "Secretaria de Estado do Índio",
+      "nomeCredor": "Wanderson Silva Farias",
+      "cpf": "3029071227",
+      "total": 2580
+    },
+    {
+      "exercicio": 2021,
+      "codigoUnidadeorcamentaria": "30101",
+      "descricaoUnidadeOrcamentaria": "Secretaria de Estado de Articulação Municipal e Política Urbana",
+      "nomeCredor": "Jose Alex Soares Ferreira",
+      "cpf": "76116646204",
+      "total": 1380
+    },
+    {
+      "exercicio": 2021,
+      "codigoUnidadeorcamentaria": "32101",
+      "descricaoUnidadeOrcamentaria": "Defensoria Pública do Estado de Roraima",
+      "nomeCredor": "Stélio Dener de Souza Cruz",
+      "cpf": "38306050215",
+      "total": 70688.2
+    },
+    {
+      "exercicio": 2021,
+      "codigoUnidadeorcamentaria": "33101",
+      "descricaoUnidadeOrcamentaria": "Ministério Público de Contas",
+      "nomeCredor": "Marcos Paulo Veloso Oliveira",
+      "cpf": "96889934253",
+      "total": 5332.42
+    },
+    {
+      "exercicio": 2021,
+      "codigoUnidadeorcamentaria": "34101",
+      "descricaoUnidadeOrcamentaria": "Secretaria de Estado da Cultura",
+      "nomeCredor": "Jairo Maciel da Silva",
+      "cpf": "20113102291",
+      "total": 60
+    }
+  ]
